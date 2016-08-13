05a8f64d368d83bc9b3fa520413512fdbfcfcf29
Jonatan H Sundqvist <jonatanhsundqvist@gmail.com>
2016-08-13 08:41:46 +0200

    Fixed stuff

--- programs/importer.lua
+++ programs/importer.lua
@@ -1,4 +1,9 @@
---
+-- Importing scripts from GitHub
+-- Jonatan H Sundqvist
+-- August 11(-ish) 2016
+
+-- TODO: How do you control exports within the ComputerCraft Lua environment?
+
 local path = 'https://raw.githubusercontent.com/%s/%s/%s/%s'
 local settings = { username   = 'swiftsnamesake',
 	               branch     = 'master',
@@ -6,7 +11,7 @@ local settings = { username   = 'swiftsnamesake',
 
 
 
-local function load(fn)
+function load(fn)
   --
   local url = path:format(settings.username, settings.repository, settings.branch, fn)
   if http.checkURL(url) then
@@ -14,17 +19,17 @@ local function load(fn)
     print(url)
     return response
   else
-  	print(('Forbidden url: %s':format(url)))
+  	print(('Forbidden url: %s'):format(url))
   	return nil
   end
 end
 
 
-local function save(fn, to)
+function save(fn, to)
   local response = load(fn)
   if fn ~= nil then
   	-- TODO: Check pre-existing (?)
-  	local f = fs.open(fn, 'w')
+  	local f = fs.open(to, 'w')
   	f.write(response.readAll())
   	f.close()
   else
